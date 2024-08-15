9c430e8446a2574f6ccaf7b40cd5492e265b5d27
HoangDucHiep <hunghiephainhuan1412@gmail.com>
2024-08-15 15:00:10 +0700

    fist commit + chapter02

---
+++ chapter02/hello_world.py
@@ -0,0 +1,14 @@
+""" message = "Hello Python world!"
+print(message)
+
+"""
+
+name = "ada lovelace"
+print(name.upper())
+
+number = 1
+
+first_name = "Hiep"
+last_name = "Hoang"
+full_name = f"{first_name} {last_name} {number}"
+print(full_name)
